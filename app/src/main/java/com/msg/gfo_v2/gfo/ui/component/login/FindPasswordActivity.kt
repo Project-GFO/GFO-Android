de0daeea1273d5068e25a9df9e4daa2ab78b72e5
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-11-16 20:58:59 +0900

    LoginActivity 관련 화면전환 구현

---
+++ app/src/main/java/com/msg/gfo_v2/gfo/ui/component/login/FindPasswordActivity.kt
@@ -0,0 +1,15 @@
+package com.msg.gfo_v2.gfo.ui.component.login
+
+import com.msg.gfo_v2.R
+import com.msg.gfo_v2.databinding.ActivityFindPasswordBinding
+import com.msg.gfo_v2.gfo.ui.base.BaseActivity
+
+class FindPasswordActivity : BaseActivity<ActivityFindPasswordBinding>(R.layout.activity_find_password) {
+    override fun viewSetting() {
+
+    }
+
+    override fun observeEvent() {
+
+    }
+}
