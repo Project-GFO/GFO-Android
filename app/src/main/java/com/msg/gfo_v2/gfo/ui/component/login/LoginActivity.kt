3c6d294ac0ed829d5f2c7edf45e4c4f5e53ff8ef
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-11-16 20:59:51 +0900

    실수로 빼놓고 커밋함

--- app/src/main/java/com/msg/gfo_v2/gfo/ui/component/login/LoginActivity.kt
+++ app/src/main/java/com/msg/gfo_v2/gfo/ui/component/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.msg.gfo_v2.gfo.ui.component.login
 
+import android.content.Intent
+import android.view.View
 import com.msg.gfo_v2.R
 import com.msg.gfo_v2.databinding.ActivityLoginBinding
 import com.msg.gfo_v2.gfo.ui.base.BaseActivity
+import com.msg.gfo_v2.gfo.ui.component.main.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
@@ -11,6 +14,25 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
     }
 
     override fun observeEvent() {
+
+    }
+
+    fun onClickPageBtn(view: View) {
+        lateinit var act: Class<*>
+        when(view.id) {
+            binding.btnLogin.id -> {
+                act = MainActivity::class.java
+                login()
+            }
+            binding.btnFindPassword.id -> {
+                act = FindPasswordActivity::class.java
+            }
+        }
+        val intent = Intent(this, act)
+        startActivity(intent)
+    }
+
+    private fun login() {
 
     }
 }
