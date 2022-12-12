7e46f3e6301429da1b304e4801dd45ed4808af65
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-12-12 20:46:11 +0900

    :art: SerializedName 추가

--- app/src/main/java/com/msg/gfo_v2/gfo/data/remote/dto/datasource/auth/response/RegisterResponse.kt
+++ app/src/main/java/com/msg/gfo_v2/gfo/data/remote/dto/datasource/auth/response/RegisterResponse.kt
@@ -1,8 +1,13 @@
 package com.msg.gfo_v2.gfo.data.remote.dto.datasource.auth.response
 
+import com.google.gson.annotations.SerializedName
+
 data class RegisterResponse(
+    @SerializedName("accessToken")
     val accessToken: String,
+    @SerializedName("refreshToken")
     val refreshToken: String,
+    @SerializedName("expiredAt")
     val expiredAt: String
 )
 
