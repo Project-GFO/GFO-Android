84787df906665af3e7dd16b68f40e8ee80103674
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2023-03-02 17:37:16 +0900

    :lipstick: Spalsh구현

--- app/src/main/java/com/msg/gfo_v2/gfo/ui/component/splash/SplashActivity.kt
+++ app/src/main/java/com/msg/gfo_v2/gfo/ui/component/splash/SplashActivity.kt
@@ -1,12 +1,17 @@
 package com.msg.gfo_v2.gfo.ui.component.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.msg.gfo_v2.R
+import com.msg.gfo_v2.gfo.ui.component.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
